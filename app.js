85b18d7a5c02d1a013389a94d654ba560df0d0a2
mDjeee <isejtniyazov@mail.ru>
2022-11-28 14:37:35 +0500

    feat: add app.js and routes.js

---
+++ app.js
@@ -0,0 +1,7 @@
+import http from 'http';
+
+import requestHandler from './routes';
+
+const server = http.createServer(requestHandler);
+
+server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,7 @@
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+}
+
+export default requestHandler;
